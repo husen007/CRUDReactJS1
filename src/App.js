d437e32adaf1f48e2afff7853923cb4ab72239d0
Royan Husen Fatih <royanhusen007@gmail.com>
2021-04-27 20:45:16 +0700

    update 1

--- src/App.js
+++ src/App.js
@@ -1,101 +1,99 @@
-import React, {Component} from 'react';
-import NavbarComponents from './components/NavbarComponents';
-import JumbotronComponents from './components/JumbotronComponents';
-import {Container} from 'reactstrap';
-import {TableComponents} from './components/TableComponents';
+import React, { Component } from "react";
+import NavbarComponents from "./components/NavbarComponents";
+import JumbotronComponents from "./components/JumbotronComponents";
+import { Container } from "reactstrap";
+import { TableComponents } from "./components/TableComponents";
 
 export default class App extends Component {
   state = {
-    title: 'Simple CRUD ReactJS',
+    title: "Simple CRUD ReactJS",
     users: [
       {
         id: 1,
-        nama: 'Royan',
-        alamat: 'Tangerang',
+        nama: "Royan",
+        alamat: "Tangerang",
         umur: 25,
-        nohp: '021111',
+        nohp: "021111",
       },
       {
         id: 2,
-        nama: 'Husen',
-        alamat: 'Serang',
+        nama: "Husen",
+        alamat: "Serang",
         umur: 21,
-        nohp: '021112',
+        nohp: "021112",
       },
       {
         id: 3,
-        nama: 'Fatih',
-        alamat: 'Tangerang',
+        nama: "Fatih",
+        alamat: "Tangerang",
         umur: 23,
-        nohp: '021113',
+        nohp: "021113",
       },
       {
         id: 4,
-        nama: 'Jaya',
-        alamat: 'Jakarta Barat',
+        nama: "Jaya",
+        alamat: "Jakarta Barat",
         umur: 23,
-        nohp: '021114',
+        nohp: "021114",
       },
       {
         id: 5,
-        nama: 'Bagus',
-        alamat: 'Tangerang',
+        nama: "Bagus",
+        alamat: "Tangerang",
         umur: 26,
-        nohp: '021115',
+        nohp: "021115",
       },
       {
         id: 6,
-        nama: 'Bagas',
-        alamat: 'Jakarta Selatan',
+        nama: "Bagas",
+        alamat: "Jakarta Selatan",
         umur: 23,
-        nohp: '021116',
+        nohp: "021116",
       },
       {
         id: 7,
-        nama: 'Indah',
-        alamat: 'Tasikmalaya',
+        nama: "Indah",
+        alamat: "Tasikmalaya",
         umur: 29,
-        nohp: '021117',
+        nohp: "021117",
       },
       {
         id: 8,
-        nama: 'Satria',
-        alamat: 'Ciamis',
+        nama: "Satria",
+        alamat: "Ciamis",
         umur: 23,
-        nohp: '021118',
+        nohp: "021118",
       },
       {
         id: 9,
-        nama: 'Agus',
-        alamat: 'Subang',
+        nama: "Agus",
+        alamat: "Subang",
         umur: 25,
-        nohp: '021119',
+        nohp: "021119",
       },
       {
         id: 10,
-        nama: 'Udin',
-        alamat: 'Tangerang',
+        nama: "Udin",
+        alamat: "Tangerang",
         umur: 24,
-        nohp: '021110',
+        nohp: "021110",
       },
       {
         id: 11,
-        nama: 'Zidan',
-        alamat: 'Bandung',
+        nama: "Zidan",
+        alamat: "Bandung",
         umur: 25,
-        nohp: '021121',
+        nohp: "021121",
       },
     ],
   };
   render() {
     return (
-      <div>
-        <Container>
-          <NavbarComponents />
-          <JumbotronComponents title={this.state.title} />
-          <TableComponents users={this.state.users} />
-        </Container>
-      </div>
+      <Container>
+        <NavbarComponents />
+        <JumbotronComponents title={this.state.title} />
+        <TableComponents users={this.state.users} />
+      </Container>
     );
   }
 }
--- src/components/TableComponents.js
+++ src/components/TableComponents.js
@@ -1,41 +1,45 @@
-import {faEdit, faInfo, faTrash} from '@fortawesome/free-solid-svg-icons';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import React from 'react';
-import BootstrapTable from 'react-bootstrap-table-next';
-import {Container, Button} from 'reactstrap';
+import { faEdit, faInfo, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import BootstrapTable from "react-bootstrap-table-next";
+import { Button } from "reactstrap";
+import ToolkitProvider, { Search } from "react-bootstrap-table2-toolkit";
+import paginationFactory from "react-bootstrap-table2-paginator";
+
+const { SearchBar } = Search;
 
 const columns = [
   {
-    dataField: 'id',
-    text: 'ID',
+    dataField: "id",
+    text: "ID",
     sort: true,
     headerStyle: () => {
-      return {width: '5%'};
+      return { width: "5%" };
     },
   },
   {
-    dataField: 'nama',
-    text: 'Name',
+    dataField: "nama",
+    text: "Name",
     sort: true,
   },
   {
-    dataField: 'alamat',
-    text: 'Alamat',
+    dataField: "alamat",
+    text: "Alamat",
     sort: true,
   },
   {
-    dataField: 'link',
-    text: 'Action',
+    dataField: "link",
+    text: "Action",
     formatter: (row) => {
       return (
         <div>
           <Button color="dark" className="mr-2">
             <FontAwesomeIcon icon={faInfo} /> Detail
           </Button>
-          <Button color="dark" className="mr-2">
+          <Button color="btn btn-success" className="mr-2">
             <FontAwesomeIcon icon={faEdit} /> Edit
           </Button>
-          <Button color="dark" className="mr-2">
+          <Button color="btn btn-danger" className="mr-2">
             <FontAwesomeIcon icon={faTrash} /> Delete
           </Button>
         </div>
@@ -46,25 +50,35 @@ const columns = [
 
 const defaultSorted = [
   {
-    dataField: ' id',
-    order: 'asc',
+    dataField: " id",
+    order: "asc",
   },
 ];
 
 export const TableComponents = (props) => {
   return (
     <div>
-      <h1>
-        <Container>
-          <BootstrapTable
-            bootstrap4
-            keyField="id"
-            data={props.users}
-            columns={columns}
-            defaultSorted={defaultSorted}
-          />
-        </Container>
-      </h1>
+      <ToolkitProvider
+        bootstrap4
+        keyField="id"
+        data={props.users}
+        columns={columns}
+        defaultSorted={defaultSorted}
+        search
+      >
+        {(props) => (
+          <div>
+            <div className="float-right">
+              <SearchBar {...props.searchProps} placeholder="Search..." />
+            </div>
+            <hr />
+            <BootstrapTable
+              {...props.baseProps}
+              pagination={paginationFactory()}
+            />
+          </div>
+        )}
+      </ToolkitProvider>
     </div>
   );
 };
